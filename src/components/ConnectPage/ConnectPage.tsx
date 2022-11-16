8dc03cf886f492d0fa4c430a52be487aea518b4e
Oshadha Gunasekara <oshadha123@gmail.com>
2022-11-16 14:14:41 -0600

    clean up dispenser state

--- src/components/ConnectPage/ConnectPage.tsx
+++ src/components/ConnectPage/ConnectPage.tsx
@@ -52,14 +52,5 @@ export default function ConnectPage(props: any) {
                 </button>
             </form>
         </div>
-        <div>
-            <h1>Dispenser</h1>
-            <form>
-                <label>IP: </label><input></input><br/>
-                <label>Username: </label><input></input><br/>
-                <label>Password: </label><input></input><br/>
-                <button>Connect Dispenser</button>
-            </form>
-        </div>
     </div>
 }
--- src/components/StatePage/DispenserState/DispenserState.tsx
+++ src/components/StatePage/DispenserState/DispenserState.tsx
@@ -8,23 +8,23 @@ export default function DispenserState() {
     const [connected, setConnected] = useState(false);
 
     // check for dispenser status every second
-    useEffect(() => {
-        const axios = require('axios');
+    // useEffect(() => {
+    //     const axios = require('axios');
 
-        const interval = setInterval(() => {
-            axios.get(DISPENSER_STATUS_URI)
-            .then((response: any) => {
-                setConnected(true);
-                setBottleCount(response.data.bottleCount);
-            })
-            .catch((error: any) => {
-                setConnected(false);
-                setBottleCount("who knows");
-            });
-        }, 1000);
+    //     const interval = setInterval(() => {
+    //         axios.get(DISPENSER_STATUS_URI)
+    //         .then((response: any) => {
+    //             setConnected(true);
+    //             setBottleCount(response.data.bottleCount);
+    //         })
+    //         .catch((error: any) => {
+    //             setConnected(false);
+    //             setBottleCount("who knows");
+    //         });
+    //     }, 1000);
 
-        return () => clearInterval(interval);
-    }, []);
+    //     return () => clearInterval(interval);
+    // }, []);
 
     return <div>
         <h1>Dispenser</h1>
